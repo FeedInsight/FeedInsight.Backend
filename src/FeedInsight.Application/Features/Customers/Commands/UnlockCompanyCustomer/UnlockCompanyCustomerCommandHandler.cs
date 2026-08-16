4d8ef0d82321c02c4486c27e46a19c2d0c825dd2
alaaessam <ae321611@gmail.com>
2026-08-16 14:47:14 +0300

    Add company customer lock and unlock

---
+++ src/FeedInsight.Application/Features/Customers/Commands/UnlockCompanyCustomer/UnlockCompanyCustomerCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using ErrorOr;
+using FeedInsight.Application.Common.Interfaces;
+using FeedInsight.Application.Features.Users.Specifications;
+using FeedInsight.Application.Messaging;
+using FeedInsight.Domain.Common.Errors;
+using FeedInsight.Domain.Common.Interfaces;
+using FeedInsight.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeedInsight.Application.Features.Customers.Commands.UnlockCompanyCustomer
+{
+    public class UnlockCompanyCustomerCommandHandler: IRequestHandler<UnlockCompanyCustomerCommand, ErrorOr<Success>>
+    {
+        private readonly IRepository<User> _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UnlockCompanyCustomerCommandHandler(
+            IRepository<User> userRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<ErrorOr<Success>> HandleAsync(
+            UnlockCompanyCustomerCommand request,
+            CancellationToken cancellationToken = default)
+        {
+            var customer = await _userRepository.SingleOrDefaultAsync(
+                new UserByIdSpec(request.CustomerId),
+                cancellationToken);
+
+            if (customer is null)
+                return Errors.Users.NotFound;
+
+            if (!customer.HasRole(Role.CompanyCustomer))
+                return Errors.Users.NotCustomer;
+
+            customer.UnlockAccount();
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Success;
+        }
+    }
+}
