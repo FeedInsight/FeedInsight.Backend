31d28f3f049a173fa6aaad702575d7cd9b941b29
alaaessam <ae321611@gmail.com>
2026-08-19 16:34:23 +0300

    modify the snapshots analytics

--- src/FeedInsight.Application/Features/Analytics/Queries/Specifications/ExtractedTasksByTenantAndDateSpec.cs
+++ src/FeedInsight.Application/Features/Analytics/Queries/Specifications/ExtractedTasksByTenantAndDateSpec.cs
@@ -10,15 +10,12 @@ namespace FeedInsight.Application.Features.Analytics.Queries.Specifications
     public class ExtractedTasksByTenantAndDateSpec : Specification<ExtractedTask>
     {
         public ExtractedTasksByTenantAndDateSpec(
-            Guid tenantId,
-            DateTime startDate,
-            DateTime endDate)
+       Guid tenantId,
+       DateTime endDate)
         {
-            Query
-                .Where(x =>
-                    x.TenantId == tenantId &&
-                    x.CreatedAt >= startDate &&
-                    x.CreatedAt < endDate);
+            Query.Where(x =>
+                x.TenantId == tenantId &&
+                x.CreatedAt < endDate);
         }
     }
 }
